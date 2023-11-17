c9b14f8e6203253f3f354d8740a89eb9d50f2377
wDaaV <150930604+wDaaV@users.noreply.github.com>
2023-11-17 10:23:22 +0000

    aggiunta commenti+ codice cookie(commentato)

--- seebeyond/src/App.js
+++ seebeyond/src/App.js
@@ -4,6 +4,7 @@ import Info from './components/info'; // import Info component
 import OCR from './components/ocr'; // import OCR component
 import Maps from './components/maps'; // import Maps component
 
+//import delle immagini
 import InfoImage from './images/Info.png';
 import OCRImage from './images/OCR.png';
 import MapsImage from './images/Maps.png';
@@ -11,10 +12,37 @@ import VoiceImage from './images/Comando Vocale.png';
 import OnOffImage from './images/OnOff.png';
 
 function App(){
+  
+  /*
+  //cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  //fine cookie
+  */
+
   const [ActivePage, setActivePage] = useState('Home');
 
   const [isActive, setIsActive] = useState(false);
       
+  //definizione attributi bottone
   const buttons = [
     { id: 1, name: 'top-left', state: isActive, img: InfoImage },
     { id: 2, name: 'top-right', state: false, img: OCRImage },
@@ -30,6 +58,7 @@ function App(){
 
   let activePage;
 
+  //sitch per assegnare ad ActivePage la pagina attiva
   switch(ActivePage){
     case 'Home':
       activePage = <Home  
--- seebeyond/src/index.js
+++ seebeyond/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//pagina di default che punta alla pagina "App"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
