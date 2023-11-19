57e6dc2bd5153d56af322ea1a763230141f9b5d3
wDaaV <150930604+wDaaV@users.noreply.github.com>
2023-11-19 18:19:48 +0000

    aggiunta assistente vocale

--- WebApp React/src/components/voice.js
+++ WebApp React/src/components/voice.js
@@ -1,18 +1,65 @@
 import '../style/voice.css'
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function Voice({ActivePage, onActivePage}){
+const Voice = ({ActivePage, onActivePage}) => {
 
     const handleGoBack = () => {
         onActivePage('Home');
     }
 
-    //
-    //
+    const commands = [
+        {
+            command: 'Torna indietro',
+            callback:() => handleGoBack()
+        },
+        {
+            command: 'Torna alla home',
+            callback:() => handleGoBack()
+        },
+        {
+            command: 'Torna alla pagina precedente',
+            callback:() => handleGoBack()
+        },
+        {
+            command: 'Apri Home',
+            callback:() => handleGoBack()
+        },
+        {
+            command: 'Apri pagina Home',
+            callback:() => handleGoBack()
+        },
+        {
+            command: 'Apri Maps',
+            callback:() => onActivePage('Maps')//window.open("https://maps.google.com")
+        },
+        {
+            command: 'Apri Info',
+            callback:() => onActivePage('Info')
+        }
+    ]
 
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition({commands}); 
+    
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
     return(
         <>
         <div className="information">
             <h1>Assistente Vocale</h1>
+        </div>
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button onClick={() => SpeechRecognition.startListening({continuous: true, language: 'it-IT'})}>Start</button>
+            <button onClick={() => SpeechRecognition.stopListening({continuous: false})}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
         </div>
         <div className="buttonContainer">
             <button className="GoBackContainer">
