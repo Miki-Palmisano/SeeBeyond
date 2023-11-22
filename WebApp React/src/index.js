b8d90e6317a97712e1a9b28b0e70691cef87a2b6
wDaaV <150930604+wDaaV@users.noreply.github.com>
2023-11-22 09:36:55 +0000

    aggiunta parziale firebase

--- WebApp React/src/index.js
+++ WebApp React/src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAux78sUVoNRz7WCmEMZTdhTtAQUzKP19M",
+  authDomain: "seebeyond-b370a.firebaseapp.com",
+  projectId: "seebeyond-b370a",
+  storageBucket: "seebeyond-b370a.appspot.com",
+  messagingSenderId: "658711291124",
+  appId: "1:658711291124:web:38ffd4c3eae0ff69bfeaa1",
+  measurementId: "G-Y5CQFT8118"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 //pagina di default che punta alla pagina "App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
