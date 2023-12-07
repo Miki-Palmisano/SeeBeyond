f8a79af889a1c217bbaf8bdd97c18084265d2d4f
Miki-Palmisano <palmmiki21@gmail.com>
2023-12-07 15:30:35 +0100

    Fix Voice Assistant

--- src/components/voice.js
+++ src/components/voice.js
@@ -2,6 +2,8 @@ import '../style/voice.css'
 import React, {useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
+const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
 const Voice = ({ActivePage, onActivePage}) => {
 
     const [isActive, setIsActive] = useState(false); //ceck bottone avvia/stop assistente
