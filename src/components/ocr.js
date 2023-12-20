62e9c9c2b0fc81d4c0539ce06c5333524174aecf
Miki-Palmisano <palmmiki21@gmail.com>
2023-12-20 19:37:54 +0100

    Fix iPhone TTS OCR

--- src/components/ocr.js
+++ src/components/ocr.js
@@ -1,6 +1,6 @@
 import '../style/ocr.css'
-import React, {useState} from 'react';
-import { getDatabase, ref, child, get } from "firebase/database";
+import React, {useState, useEffect} from 'react';
+import { getDatabase, ref, child, get, set } from "firebase/database";
 
 function OCR({ActivePage, onActivePage, database}){
 
@@ -8,15 +8,18 @@ function OCR({ActivePage, onActivePage, database}){
         onActivePage('Home');
     }
 
-    const [isActive, setIsActive] = useState(true); //check bottone on/off AI, non usato al momento (bottone non presente per ora)
+    const [isActive, setIsActive] = useState(false); //check bottone on/off AI, non usato al momento (bottone non presente per ora)
 
-    const readDBText = () => {
-        const dbRef = ref(database);
-        get(child(dbRef, `Testo Rilevato/Testo`)).then((snapshot) => {
-            let utterance = new SpeechSynthesisUtterance(snapshot.val());
-            window.speechSynthesis.speak(utterance);
-        }).catch((error) => { console.error(error); });
-    }
+    useEffect(() => {
+        if(isActive){
+            const dbRef = ref(database);
+            get(child(dbRef, `Testo Rilevato/Testo`)).then((snapshot) => {
+                let utterance = new SpeechSynthesisUtterance(snapshot.val());
+                window.speechSynthesis.speak(utterance);
+            }).catch((error) => { console.error(error); });
+            setIsActive(false);
+        }
+    }, [isActive]);
 
     return(
         <>
@@ -25,7 +28,7 @@ function OCR({ActivePage, onActivePage, database}){
                     <h1 className="OcrH1">OCR</h1>
                 </div>
                 <button className="OcrContainer">
-                    <h1 className="OcrButton" onClick={() => { readDBText(); }}>AVVIA OCR</h1>
+                    <h1 className="OcrButton" onClick={() => { setIsActive(true); }}>AVVIA OCR</h1>
                 </button>
                 <div className="OcrButtonContainer">
                     <button className="OcrGoBackContainer">
