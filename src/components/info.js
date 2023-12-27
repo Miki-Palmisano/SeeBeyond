679b21ffd2eacb89b827023a8eba44002c7145c2
Miki-Palmisano <palmmiki21@gmail.com>
2023-12-27 19:42:40 +0100

    Update TTS Info Guide

--- src/components/info.js
+++ src/components/info.js
@@ -1,23 +1,40 @@
+import { useEffect, useState } from 'react';
 import '../style/info.css'
+import { set } from 'firebase/database';
 
 function Info ({ActivePage, onActivePage}){
 
     const handleGoBack = () => {
         onActivePage('Home');
+        window.speechSynthesis.cancel();
     }
+
+    const [info, setInfo] = useState(`SeeBeyond è un progetto, ideato da un gruppo di studenti del politecnico di Bari, rivolto alle persone con acuità visiva che cercano soluzioni innovative ed efficaci per orientarsi in qualsiasi tipo di ambiente.
+    L'app è strutturata in 6 funzioni, in ordine puoi trovare:
+    - Informazioni Generali cliccando in alto a sinistra;
+    - Lettura testo riconosciuto dall'OCR cliccando in alto a destra;
+    - Assistente Vocale cliccando in basso a destra;
+    - Assistente di Navigazione cliccando in basso a sinistra;
+    - Abilitare o Disabilitare lettura oggetti cliccando due volte al centro;
+    - Spegnere o Accendere l'app cliccando al centro una sola volta;`
+    );
+    
+
+    useEffect(() => {
+        let utterance = new SpeechSynthesisUtterance(info);
+        window.speechSynthesis.speak(utterance);
+        return () => {
+            window.speechSynthesis.cancel();
+        };
+    }, []);
+    
 
     return(
         <>
         <div className="InfoBody">
             <div className="InfoInformation">
                 <h1 className="InfoH1">INFO PAGE</h1>
-                <p className="InfoP">
-                    SeeBeyond &egrave; un progetto, ideato da un gruppo di studenti del politecnico di Bari, rivolto alle persone con acuit&agrave; visiva che cercano soluzioni innovative ed efficaci per orientarsi in qualsiasi tipo di ambiente.<br/>
-                    Funzionalit&agrave; dell'App:<br/>
-                    - Riconoscimento di oggetti e testo (doppio-tap sul bottone di accensione per attivare/disattivare)<br/>
-                    - Assistente vocale personalizzato per muoversi nell'app e per informazioni utili<br/>
-                    - Assistenza alla mobilit&agrave; programmando il miglior percorso a piedi per la destinazione inserita<br/>
-                </p>
+                <p className="InfoP" dangerouslySetInnerHTML={{__html: info.replace(/(?:\r\n|\r|\n)/g, '<br>')}}></p> 
             </div>
             <div className="InfoButtonContainer">
                 <button className="InfoGoBackContainer">
